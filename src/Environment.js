d2c05183d2bdc5a0f63245cc81bb49b54d34e500
Nate Flood <nate@famconnect.net>
2020-04-19 22:15:04 -0400

    Set ACCEPT-LANGUAGE

--- src/Environment.js
+++ src/Environment.js
@@ -1,20 +1,39 @@
-const { Environment, Network, RecordSource, Store } = require('relay-runtime')
-const store = new Store(new RecordSource())
-const network = Network.create((operation, variables) => {
-  return fetch('http://development.echocommunity.org:3000/graphql/', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: operation.text,
-      variables,
+const { Environment, RecordSource, Store } = require('relay-runtime')
+const store = new Store(new RecordSource());
+const { RelayNetworkLayer, urlMiddleware } = require('react-relay-network-modern/node8');
+
+
+// const network = Network.create((operation, variables) => {
+//   return fetch('http://development.echocommunity.org:3000/graphql/', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       query: operation.text,
+//       variables,
+//     }),
+//   }).then(response => {
+//     return response.json()
+//   })
+// })
+
+const network = new RelayNetworkLayer(
+  [
+    urlMiddleware({
+      url: (req) => Promise.resolve('http://development.echocommunity.org:3000/graphql/'),
     }),
-  }).then(response => {
-    return response.json()
-  })
-})
+    (next) => async (req) => {
+      req.fetchOpts.headers['ACCEPT-LANGUAGE'] = 'es';
+      const res = await next(req);
+      return res;
+    },
+  ],
+  // opts
+);
+
+
 const environment = new Environment({
   network,
   store,
